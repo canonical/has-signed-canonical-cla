abaf86489b9f896a3e4c59071b87f9b211645c3c
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2021-03-18 11:27:10 +0000

    Fix crash

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-async function run()
-{
+async function run() {
   const username = core.getInput('username', { required: true });
   const token = core.getInput('token', { required: true });
-  
+
   const octokit = github.getOctokit(token);
-  
+
   const { data: orgs } = await octokit.orgs.listForUser({ username, per_page: 100 });
 
-  const result = await octokit.request('GET /orgs/{org}/members/{username}', {
+  await octokit.request('GET /orgs/{org}/members/{username}', {
     org: 'CanonicalContributorAgreement',
     username: username
+  }).then((result) => {
+    core.setOutput('has_signed', result.status == 204);
+  }).catch((error) => {
+    core.setOutput('has_signed', false);
   });
-  
-  const has_signed = (result.status == 204);
-  
-  core.setOutput('has_signed', has_signed);
 }
 
 run();
