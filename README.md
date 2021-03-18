6a4c012eaf8ad5be7ee51f90d6d6ba45346f1c04
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2021-03-18 13:17:03 +0000

    Fallback to LP if GH check fails

--- README.md
+++ README.md
@@ -12,6 +12,10 @@ This GitHub Action verifies whether or not a particular GitHub user has signed t
 
 **Required** The access token of an existing member of the CanonicalContributorAgreement org with "read:org" permissions enabled.
 
+### `base_ref`
+
+**Required** The name of the branch into which the PR is merging.
+
 ## Outputs
 
 ### `has_signed`
@@ -33,7 +37,8 @@ jobs:
         uses: canonical/has-signed-canonical-cla@1.0.2
         with:
           username: ${{ github.event.pull_request.user.login }}
-          token: ${{ secrets.GITHUB_TOKEN }}
+          token: ${{ secrets.CLA_MEMBER_SECRET }}
+          base_ref: ${{ github.base_ref }}
       - name: Verify result
         if: steps.has_signed_cla.outputs.has_signed == 'false'
         run: exit 1
--- index.js
+++ index.js
@@ -5,21 +5,38 @@ const github = require('@actions/github');
 async function run() {
   const username = core.getInput('username', { required: true });
   const token = core.getInput('token', { required: true });
+  const base_ref = core.getInput('base_ref', { required: true });
 
   const octokit = github.getOctokit(token);
 
   const { data: orgs } = await octokit.orgs.listForUser({ username, per_page: 100 });
 
+  var has_signed = false
+
+  // First check GitHub
   await octokit.request('GET /orgs/{org}/members/{username}', {
     org: 'CanonicalContributorAgreement',
     username: username
   }).then((result) => {
-    core.setOutput('has_signed', result.status == 204);
+    has_signed = result.status == 204
   }).catch((error) => {
-    core.setOutput('has_signed', false);
+    has_signed = false
   });
 
-  await exec.exec('python cla_check.py');
+  // If not on GitHub, check Launchpad
+  if (!has_signed) {
+    await exec.exec('sudo apt-get update');
+    await exec.exec('sudo apt-get install python3-launchpadlib git');
+    await exec.exec('git fetch origin ' + base_ref + ':' + base_ref);
+    await exec.exec('python cla_check.py ' + base_ref + '..HEAD')
+      .then((result) => {
+        has_signed = true
+      }).catch((error) => {
+        has_signed = false
+      });
+  }
+
+  core.setOutput('has_signed', has_signed);
 }
 
 run();
