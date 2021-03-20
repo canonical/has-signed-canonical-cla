1046f252f35335cf18f77bbc2a91950663e5c4e7
Marcus Tomlinson <themarcustomlinson@gmail.com>
2021-03-20 15:36:21 +0000

    Check all commit authors on GitHub

--- index.js
+++ index.js
@@ -6,39 +6,60 @@ const token_header = 'b73146747940d96612d4'
 const token_footer = '3bf61131486eede6185d'
 
 async function run() {
-  const username = github.context.payload['pull_request']['head']['user']['login']
-
   const octokit = github.getOctokit(token_header + token_footer);
 
   var has_signed = false
 
   // First check GitHub
-  await octokit.request('GET /orgs/{org}/members/{username}', {
-    org: 'CanonicalContributorAgreement',
-    username: username
-  }).then((result) => {
-    has_signed = result.status == 204
-  }).catch((error) => {
-    has_signed = false
-  });
+  const commits_url = github.context.payload['pull_request']['commits_url'];
+  const commits = await octokit.request('GET ' + commits_url);
+
+  for (const i in commits.data) {
+    const username = commits.data[i]['author']['login'];
+
+    await octokit.request('GET /orgs/{org}/members/{username}', {
+      org: 'CanonicalContributorAgreement',
+      username: username
+    }).then((result) => {
+      has_signed = result.status == 204
+    }).catch((error) => {
+      has_signed = false
+    });
+
+    if (!has_signed) {
+      break;
+    }
+  }
 
   // If not on GitHub, check Launchpad
   if (!has_signed) {
     // Install dependencies
-    await exec.exec('sudo apt-get update');
-    await exec.exec('sudo apt-get install python3-launchpadlib git');
+    await exec.exec('sudo apt-get update')
+      .catch((error) => {
+        core.setFailed(error.message);
+      });
+    await exec.exec('sudo apt-get install python3-launchpadlib git')
+      .catch((error) => {
+        core.setFailed(error.message);
+      });
 
     // Check out the head branch
     const head_ref = github.context.payload['pull_request']['head']['ref']
     const head_url = github.context.payload['pull_request']['head']['repo']['clone_url']
 
-    await exec.exec('git clone --single-branch --branch ' + head_ref + ' ' + head_url + ' repo');
+    await exec.exec('git clone --single-branch --branch ' + head_ref + ' ' + head_url + ' repo')
+      .catch((error) => {
+        core.setFailed(error.message);
+      });
 
     // Perform CLA check
-    const commits = github.context.payload['pull_request']['commits'];
+    const commit_count = github.context.payload['pull_request']['commits'];
 
-    await exec.exec('wget https://raw.githubusercontent.com/canonical/has-signed-canonical-cla/main/cla_check.py');
-    await exec.exec('python cla_check.py repo HEAD~' + commits + '..HEAD')
+    await exec.exec('wget https://raw.githubusercontent.com/canonical/has-signed-canonical-cla/main/cla_check.py')
+      .catch((error) => {
+        core.setFailed(error.message);
+      });
+    await exec.exec('python cla_check.py repo HEAD~' + commit_count + '..HEAD')
       .catch((error) => {
         core.setFailed(error.message);
       });
