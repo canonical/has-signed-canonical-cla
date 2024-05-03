046337b42822b7868ad62970988929c79f9c1d40
Maksim Beliaev <beliaev.m.s@gmail.com>
2024-05-03 15:27:38 +0200

    Remove self signed certs support (#45)
    * Remove self signed certs support
    
    * Update index.js
    
    * Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const github = require('@actions/github');
+const axios = require('axios');
 const path = require('path');
 
 const githubToken = core.getInput('github-token', {required: true})
@@ -110,27 +111,21 @@ async function run() {
       continue
     }
 
-    const axios = require('axios');
-    const https = require('https');
-    // remove agent when nginx will be fixed and will provide CA certs
-    const agent = new https.Agent({
-      rejectUnauthorized: false // Ignore certificate verification
-    });
-
     try {
       console.log('Check in the signed list service');
       const response = await axios.get(
-        'https://cla-checker.canonical.com/check_user/' + username,
-        { httpsAgent: agent }
+        'https://cla-checker.canonical.com/check_user/' + username
       );
       if (response.status === 200) {
         console.log('- ' + username + ' ✓ (has signed the CLA)');
         commit_authors[i]['signed'] = true;
-      } else {
-        console.log('- ' + username + ' ✕ (has not signed the CLA)');
       }
     } catch (error) {
-      console.error('Error occurred while checking user:', error.message);
+      if (error.response && error.response.status === 404) {
+        console.log('- ' + username + ' ✕ (has not signed the CLA)');
+      } else {
+        console.error('Error occurred while checking user:', error.message);
+      }
     }
   }
 
